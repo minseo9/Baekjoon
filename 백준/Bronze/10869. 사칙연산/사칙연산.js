2d61230c378218685910d64b3be6179ecff6132f
김민서 <112479333+minseo9@users.noreply.github.com>
2025-06-19 17:04:42 +0900

    [Bronze V] Title: 사칙연산, Time: 116 ms, Memory: 9764 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,22 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let data = [];
+
+rl.on("line", function (line) {
+    data = line.split(" ").map((el) => parseInt(el));
+    rl.close();
+}).on("close", function () {
+    let num1 = data[0];
+    let num2 = data[1];
+
+    console.log(num1 + num2);
+    console.log(num1 - num2);
+    console.log(num1 * num2);
+    console.log(Math.floor(num1 / num2));
+    console.log(num1 % num2);
+});
